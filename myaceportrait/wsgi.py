f8044f311cbc979a7885b71d8df27dbaf2a8a22d
bradyjibanez <bradyjibanez@gmail.com>
2020-06-18 14:14:05 -0400

    uwsgi changes

--- myaceportrait/wsgi.py
+++ myaceportrait/wsgi.py
@@ -8,12 +8,11 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os, sys
+from django.core.wsgi import get_wsgi_application
 
 path = '/root/myACEportrait/myaceportrait'
 if path not in sys.path:
         sys.path.append(path)
-
-from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 
